1a3e87681cdbc1aeefe1b1297df5dda17eae1cd7
Max Goldstein <maxgoldstein1@gmail.com>
2015-10-13 19:25:46 -0400

    Some changes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,24 @@
 class CartsController < ApplicationController
+  # The joys of not caring about security because it's a demo
+  skip_before_filter :verify_authenticity_token
+
   def new
     @items = Item.all
+  end
+
+  def create
+    items = params.select{|k, v| k.start_with?(item_prefix) && v != "0"}
+    redirect_to :new_cart if items.empty?
+    cart = Cart.create
+    items.each do |k, v|
+      CartItem.create(cart: cart, item: k[item_prefix.length..-1].to_i, number: k.to_i)
+    end
+    puts "+++++++++++++++++++++++++++++++"
+    puts "Created #{items.length} new cart items"
+    puts "==============================="
+  end
+
+  def item_prefix
+    "item."
   end
 end
